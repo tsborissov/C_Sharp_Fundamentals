7d38a7f864d629b971defc7f6f189c29064f82b6
Tsvetan Borisov <tsborissov@gmail.com>
2020-07-30 15:11:11 +0300

    Update Program.cs

--- Programming Fundamentals Final Exam Retake - 13 December 2019/01. Warrior's Quest/Program.cs
+++ Programming Fundamentals Final Exam Retake - 13 December 2019/01. Warrior's Quest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01._Warrior_s_Quest
 {
@@ -6,8 +7,59 @@ namespace _01._Warrior_s_Quest
     {
         static void Main(string[] args)
         {
-            
 
+            string skill = Console.ReadLine();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "For Azeroth")
+                {
+                    break;
+                }
+
+                string[] command = input.Split().ToArray();
+
+                switch (command[0])
+                {
+                    case "GladiatorStance":
+
+                        skill = skill.ToUpper();
+
+                        Console.WriteLine(skill);
+
+                        break;
+
+                    case "DefensiveStance":
+
+                        skill = skill.ToLower();
+
+                        Console.WriteLine(skill);
+
+                        break;
+
+                    case "Dispel":
+
+
+
+
+                        break;
+
+                    case "Target":
+
+                        if (command[1] == "Change")
+                        {
+
+                        }
+                        else if (command [1] == "Remove")
+                        {
+
+                        }
+
+
+                        break;
+                }
+            }
 
         }
     }
