57231375dc16a78d8e5512a995507a50dce18988
Tsvetan Borisov <tsborissov@gmail.com>
2020-07-30 19:46:32 +0300

    Boss Rush problem solution

--- Programming Fundamentals Final Exam Retake - 13 December 2019/02.Boss Rush/Program.cs
+++ Programming Fundamentals Final Exam Retake - 13 December 2019/02.Boss Rush/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace _02.Boss_Rush
 {
@@ -6,8 +7,29 @@ namespace _02.Boss_Rush
     {
         static void Main(string[] args)
         {
-            
 
+            string pattern = @"\|(?<boss>[A-Z]{4,})\|:#(?<title>[A-Za-z]+ [A-Za-z]+)#";
+
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (Regex.IsMatch(input, pattern))
+                {
+                    string name = Regex.Match(input, pattern).Groups["boss"].ToString();
+                    string title = Regex.Match(input, pattern).Groups["title"].ToString();
+
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($">> Strength: {name.Length}");
+                    Console.WriteLine($">> Armour: {title.Length}");
+                }
+                else
+                {
+                    Console.WriteLine("Access denied!");
+                }
+            }
 
         }
     }
