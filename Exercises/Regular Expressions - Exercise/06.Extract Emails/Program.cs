72a1c9f419806ebf0e8da2b79c2e17b991fb3273
Tsvetan Borisov <tsborissov@gmail.com>
2020-08-17 20:11:50 +0300

    Final Exam Retake REGEX Practise

---
+++ Exercises/Regular Expressions - Exercise/06.Extract Emails/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string emailPattern = @"(?<=\s)(?<username>[a-zA-Z0-9]+[.\-_a-zA-Z0-9]*(?<=[a-zA-Z0-9]))@(?<hostname>[a-zA-Z0-9]+[.\-_a-zA-Z0-9]*(?<=[a-zA-Z0-9])\.[a-z]{2,})";
+
+            string inputText = Console.ReadLine();
+
+            MatchCollection emails = Regex.Matches(inputText, emailPattern);
+
+            foreach (Match email in emails)
+            {
+                string username = email.Groups["username"].ToString();
+                string hostname = email.Groups["hostname"].ToString();
+                
+                Console.WriteLine($"{username}@{hostname}");
+            }
+        }
+    }
+}
